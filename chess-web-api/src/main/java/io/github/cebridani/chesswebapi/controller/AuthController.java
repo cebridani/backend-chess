597ed53e4eb96b85b896507f4b0ca55f5bea6fa6
cebridani <123812489+cebridani@users.noreply.github.com>
2023-04-09 14:03:21 +0200

    local kubernetes

---
+++ chess-web-api/src/main/java/io/github/cebridani/chesswebapi/controller/AuthController.java
@@ -0,0 +1,42 @@
+package io.github.cebridani.chesswebapi.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.cebridani.chesswebapi.payload.JWTAuthResponse;
+import io.github.cebridani.chesswebapi.payload.LoginDto;
+import io.github.cebridani.chesswebapi.payload.RegisterDto;
+import io.github.cebridani.chesswebapi.service.AuthService;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    // Build Login REST API
+    @PostMapping(value = {"/login", "/signin"})
+    public ResponseEntity<JWTAuthResponse> login(@RequestBody LoginDto loginDto){
+    	String token = authService.login(loginDto);
+
+        JWTAuthResponse jwtAuthResponse = new JWTAuthResponse();
+        jwtAuthResponse.setAccessToken(token);
+
+        return ResponseEntity.ok(jwtAuthResponse);
+    }
+
+    // Build Register REST API
+    @PostMapping(value = {"/register", "/signup"})
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto){
+        String response = authService.register(registerDto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+}
---
+++ chess-web-api/src/main/java/io/github/cebridani/chesswebapi/controller/ChessController.java
@@ -0,0 +1,53 @@
+package io.github.cebridani.chesswebapi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+import io.github.cebridani.chesswebapi.payload.FenDto;
+import io.github.cebridani.chesswebapi.service.ChessService;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/chess")
+public class ChessController {
+
+    private final ChessService chessService;
+
+    public ChessController(ChessService chessService) {
+        this.chessService = chessService;
+    }
+
+    @PostMapping("/get_best_move")
+    public ResponseEntity<String> sendFen(@RequestBody FenDto fenDto, @RequestHeader("Authorization") String token) throws JsonMappingException, JsonProcessingException {
+        String bestMove = chessService.processFen(fenDto, token);
+        return ResponseEntity.ok(bestMove);
+    }
+    
+    @GetMapping("/get_fen")
+    public ResponseEntity<FenDto> getFen(@RequestHeader("Authorization") String token) {
+        FenDto fenByUser = chessService.getFenByUser(token);
+        return ResponseEntity.ok(fenByUser);
+    }
+    
+    @PostMapping("/set_fen")
+    public ResponseEntity<String> setFen(@RequestBody FenDto fenDto, @RequestHeader("Authorization") String token) {
+        chessService.setFenByUser(fenDto, token);
+        return ResponseEntity.ok("Fen setted");
+    }
+    
+    @PostMapping("/top_moves")
+    public ResponseEntity<String> top_moves(@RequestBody FenDto fenDto, @RequestHeader("Authorization") String token) throws JsonMappingException, JsonProcessingException {
+    	String response = chessService.list_best_moves(fenDto, token);
+        return ResponseEntity.ok(response);
+    }
+}
+
