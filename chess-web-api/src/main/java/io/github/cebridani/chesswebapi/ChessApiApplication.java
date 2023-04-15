0add5dfe6a361cb7f6d1c73d502d4f2c8a57c13f
cebridani <123812489+cebridani@users.noreply.github.com>
2023-04-15 15:54:11 +0200

    Update ChessApiApplication.java

--- chess-web-api/src/main/java/io/github/cebridani/chesswebapi/ChessApiApplication.java
+++ chess-web-api/src/main/java/io/github/cebridani/chesswebapi/ChessApiApplication.java
@@ -3,7 +3,7 @@ package io.github.cebridani.chesswebapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@@SpringBootApplication
+@SpringBootApplication
 public class ChessApiApplication {
 
 	public static void main(String[] args) {
