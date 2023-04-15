00f0a10e930b49b5bdcda5994be90223dff02b4d
cebridani <123812489+cebridani@users.noreply.github.com>
2023-04-15 15:28:22 +0200

    Update ChessApiApplication.java

--- chess-web-api/src/main/java/io/github/cebridani/chesswebapi/ChessApiApplication.java
+++ chess-web-api/src/main/java/io/github/cebridani/chesswebapi/ChessApiApplication.java
@@ -2,12 +2,26 @@ package io.github.cebridani.chesswebapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ChessApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChessApiApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/auth/signin").allowedOrigins("http://192.168.49.2:31621/signin");
+			}
+		};
 	}
 
 }
