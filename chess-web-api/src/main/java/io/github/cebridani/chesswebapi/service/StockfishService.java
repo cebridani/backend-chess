2b8998b248bd110296cc02639b6d7c85902c4e86
cebridani <123812489+cebridani@users.noreply.github.com>
2023-04-17 18:34:23 +0200

    Update StockfishService.java

--- chess-web-api/src/main/java/io/github/cebridani/chesswebapi/service/StockfishService.java
+++ chess-web-api/src/main/java/io/github/cebridani/chesswebapi/service/StockfishService.java
@@ -3,41 +3,75 @@ package io.github.cebridani.chesswebapi.service;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.OutputStreamWriter;
 
 public class StockfishService {
 
-    private Process stockfishProcess;
-    private PrintWriter out;
-    private BufferedReader in;
+    private Process process;
+    private BufferedReader processReader;
+    private OutputStreamWriter processWriter;
 
-    public void start() throws IOException {
-        String stockfishPath = "/usr/games/stockfish";
+    public boolean startEngine(String stockfishPath) {
         ProcessBuilder processBuilder = new ProcessBuilder(stockfishPath);
-        stockfishProcess = processBuilder.start();
-        out = new PrintWriter(stockfishProcess.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(stockfishProcess.getInputStream()));
-    }
-
-    public void stop() throws IOException {
-        in.close();
-        out.close();
-        stockfishProcess.destroy();
-    }
-
-    public String sendCommand(String command) throws IOException {
-        out.println(command);
-        StringBuilder response = new StringBuilder();
-        String line;
-
-        while ((line = in.readLine()) != null) {
-            response.append(line).append("\n");
-            if (line.contains("bestmove")) {
-                break;
-            }
+        try {
+            process = processBuilder.start();
+            processReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            processWriter = new OutputStreamWriter(process.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
         }
+        return true;
+    }
 
-        return response.toString();
+    public void stopEngine() {
+        try {
+            sendCommand("quit");
+            processReader.close();
+            processWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendCommand(String command) {
+        try {
+            processWriter.write(command + "\n");
+            processWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public String getOutput(int waitTime) {
+        StringBuilder buffer = new StringBuilder();
+        try {
+            Thread.sleep(waitTime);
+            while (processReader.ready()) {
+                buffer.append(processReader.readLine()).append("\n");
+            }
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+        return buffer.toString();
+    }
+    
+    public String getBestMove(String fen, int searchDepth) {
+        sendCommand("position fen " + fen);
+        sendCommand("go depth " + searchDepth);
+        String output = getOutput(5000);
+
+        // Encuentra la mejor jugada en la salida del motor
+        String bestMoveLine = output.lines()
+                .filter(line -> line.startsWith("bestmove"))
+                .findFirst()
+                .orElse(null);
+
+        if (bestMoveLine != null) {
+            return bestMoveLine.split(" ")[1];
+        } else {
+            return null;
+        }
     }
 }
 
