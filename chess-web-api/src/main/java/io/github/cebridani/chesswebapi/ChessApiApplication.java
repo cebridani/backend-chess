225a4e02d918c465ca0451a8530c9466c4eda15b
cebridani <123812489+cebridani@users.noreply.github.com>
2023-04-17 17:43:35 +0200

    Update ChessApiApplication.java

--- chess-web-api/src/main/java/io/github/cebridani/chesswebapi/ChessApiApplication.java
+++ chess-web-api/src/main/java/io/github/cebridani/chesswebapi/ChessApiApplication.java
@@ -3,10 +3,22 @@ package io.github.cebridani.chesswebapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cebridani.chesswebapi.service.StockfishService;
+
 @SpringBootApplication
 public class ChessApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChessApiApplication.class, args);
+		
+		StockfishService stockfish = new StockfishService();
+		try {
+			stockfish.start();
+			System.out.println(stockfish.sendCommand("ucinewgame"));
+			System.out.println(stockfish.sendCommand("go movetime 1000"));
+			stockfish.stop();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
