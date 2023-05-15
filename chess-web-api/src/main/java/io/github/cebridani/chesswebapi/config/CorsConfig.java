0599107328f1ebfd9380fbf803b32c5c139fcb49
cebridani <123812489+cebridani@users.noreply.github.com>
2023-05-15 19:57:37 +0200

    Update CorsConfig.java

--- chess-web-api/src/main/java/io/github/cebridani/chesswebapi/config/CorsConfig.java
+++ chess-web-api/src/main/java/io/github/cebridani/chesswebapi/config/CorsConfig.java
@@ -11,8 +11,8 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 //.allowedOrigins("http://192.168.49.2:31621")
-        		.allowedOrigins("http://127.0.0.1:3000")
-        		.allowedOrigins("http://127.0.0.1:3000/*")
+        		.allowedOrigins("http://192.168.49.2:31621")
+        		.allowedOrigins("http://192.168.49.2:31621/*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization")
                 .allowCredentials(true)
