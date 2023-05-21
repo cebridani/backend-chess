9fc1660895d63f0fed825149094a7a399bf63192
cebridani <123812489+cebridani@users.noreply.github.com>
2023-05-21 18:22:52 +0200

    Update AuthService.java

--- chess-web-api/src/main/java/io/github/cebridani/chesswebapi/service/AuthService.java
+++ chess-web-api/src/main/java/io/github/cebridani/chesswebapi/service/AuthService.java
@@ -3,6 +3,9 @@ package io.github.cebridani.chesswebapi.service;
 import io.github.cebridani.chesswebapi.payload.LoginDto;
 import io.github.cebridani.chesswebapi.payload.RegisterDto;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public interface AuthService {
 	public String login(LoginDto loginDto);
 
